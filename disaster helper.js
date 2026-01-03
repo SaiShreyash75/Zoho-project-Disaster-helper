fea77de1768ae35490673ef8b3315032f4c3b0a7
Shreyash <kssaishreyash@gmail.com>
2026-01-03 12:50:19 +0530

    Add files via upload

---
+++ disaster helper.js
@@ -0,0 +1,6 @@
+
+document.getElementById("volunteerTrigger").addEventListener("click", function () {
+  document.getElementById("zohoForm").style.display = "block";
+  this.style.display = "none";
+});
+
