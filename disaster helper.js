2e2a5ee382e6c05edb5b23152abbcd05d96953a4
Shreyash <kssaishreyash@gmail.com>
2026-01-06 12:32:54 +0530

    Add files via upload

--- disaster helper.js
+++ disaster helper.js
@@ -1,6 +1,25 @@
 
-document.getElementById("volunteerTrigger").addEventListener("click", function () {
-  document.getElementById("zohoForm").style.display = "block";
-  this.style.display = "none";
-});
 
+function helpTrigger() {
+    window.location.href = "Wanthelp page.html";
+
+
+} 
+function volunteerTrigger() {
+  window.location.href = "volunteer.html";
+}
+function choice(type) {
+    if (type === "shelter") {
+        window.location.href = "shelter.html";
+    } else if (type === "food") {
+        window.location.href = "FOOD.html";
+    } else if (type === "medical") {
+        window.location.href = "medical.html";
+    }
+}
+
+  function updateShelterMap(place) {
+    const map = document.getElementById("shelterMap");
+    const query = place.replace(/ /g, "+");
+    map.src = "https://www.google.com/maps?q=" + query + "&output=embed";
+  }
